0cfe495a0eb8081950949c50aefa13625ff15aab
Sachin Bhardwaj <sachinb1750@gmail.com>
2025-03-27 10:02:36 +0530

    SessionVariables

---
+++ src/test/java/perf/SessionId.java
@@ -0,0 +1,15 @@
+package perf;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class SessionId {
+    private static final char[] CANDIDATES = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
+    private static final int LENGTH = 10;
+    static String random() {
+        StringBuilder sb = new StringBuilder(LENGTH);
+        for (int i = 0; i < LENGTH; i++) {
+            sb.append(CANDIDATES[ThreadLocalRandom.current().nextInt(CANDIDATES.length)]);
+        }
+        return sb.toString();
+    }
+}
